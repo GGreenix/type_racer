a2714dcdf9f21e1263a55e2cf708105c76b5f42e
Tal Ben-Amram <talikoob@gmail.com>
2021-01-07 20:12:16 +0200

    07.01.2021
    added event handler, keyboard input handler

--- main.py
+++ main.py
@@ -1,29 +1,50 @@
 import pygame
 
-def is_a_letter_character(ascii_value):
+
+
+
+def key_event_handler(ascii_value,current_word="",text_word=""):
     if ascii_value > 96 and ascii_value < 123:
+        
         return chr(ascii_value)
+    
+    elif ascii_value == 137:
+        end_word = lambda string_to_check,string_to_equalize: True if string_to_check == string_to_equalize else False
+        print (current_word,text_word)
+        return end_word(current_word,text_word)
+
+
 def events_handler(pygame_event):
     if pygame_event.type == pygame.QUIT:
+        global run
         run = False
     elif pygame_event.type == pygame.MOUSEBUTTONUP:
         pos = pygame.mouse.get_pos()
         print(pos)
     elif pygame_event.type == pygame.KEYDOWN:
+        global current_word
+        global text_word
+
         key = pygame.key.get_pressed()
-        print(is_a_letter_character(key.index(1)+93))
+        print(key_event_handler(key.index(1)+93,current_word=current_word,text_word=text_word))
 
 
 def main():
     dim = (550,550)
-    title = "tic tac toe"
-    run = True
+    title = "type racer"
+
+    global run
+    global current_word
+    global text_word
 
     pygame.display.set_caption(title)
     pygame.init()
 
       
     canvas = pygame.display.set_mode(dim)
+    run = True
+    current_word = ""
+    text_word = ""
 
     while run:
         for event in pygame.event.get():
